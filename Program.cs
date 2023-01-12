ea10b4ae27ba36e71f5386ed2c94827358f2ec86
ben <{username}@users.noreply.github.com>
2023-01-12 16:21:36 -0600

    Created project and added 'intro' code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace mpg_cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intro
+            Console.WriteLine(Constants.AppNameASCII);
+            Console.WriteLine(Constants.AppVersion);
+
+            // Authorization
+            // @todo
+        }
+    }
+}
